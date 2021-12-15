f56a43f489d0b33d120191a9805f662b81cbd597
fabricior0cha <fabriciorochapalmeira@gmail.com>
2021-12-15 14:34:39 -0300

    @fabricior0cha [Task: Adicionando relacionamento de entidades]

--- src/main/java/org/generation/blogPessoal/model/Postagem.java
+++ src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -43,8 +44,14 @@ public class Postagem {
 	private Date date = new java.sql.Date(System.currentTimeMillis());
 
 	@ManyToOne
+	@JoinColumn(name = "fk_tema")
 	@JsonIgnoreProperties("postagem")
 	private Tema tema;
+	
+	@ManyToOne
+	@JoinColumn(name = "fk_usuario")
+	@JsonIgnoreProperties("postagem")
+	private Usuario usuario;
 
 	public long getId() {
 		return id;
--- src/main/java/org/generation/blogPessoal/model/Usuario.java
+++ src/main/java/org/generation/blogPessoal/model/Usuario.java
@@ -1,12 +1,18 @@
 package org.generation.blogPessoal.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_usuario")
@@ -19,6 +25,10 @@ public class Usuario {
 	private @NotBlank @Size(min = 5, max = 100) String usuario;
 
 	private @NotBlank @Size(min = 5, max = 100) String senha;
+	
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("usuario")
+	private List<Postagem> postagem;
 
 	public Long getId() {
 		return id;
