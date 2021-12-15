8a0ca464f5aa9627e0db57f21b542eecc0384479
fabricior0cha <fabriciorochapalmeira@gmail.com>
2021-12-15 18:25:52 -0300

    @fabricior0cha [Task: Modificando UsuarioService]

--- src/main/java/org/generation/blogPessoal/service/UsuarioService.java
+++ src/main/java/org/generation/blogPessoal/service/UsuarioService.java
@@ -19,32 +19,50 @@ public class UsuarioService {
 	@Autowired
 	private UsuarioRepository repository;
 
-	public Usuario cadastrarUsuario(Usuario usuario) {
+	public String criptografarSenha(String senha) {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-
-		String senhaEncoder = encoder.encode(usuario.getSenha());
-		usuario.setSenha(senhaEncoder);
-
-		return repository.save(usuario);
+		return encoder.encode(senha);
 	}
 
+	public Usuario cadastrarUsuario(Usuario usuario) {
+		Optional<Usuario> optional = repository.findByUsuario(usuario.getUsuario());
+		if (optional.isEmpty()) {
+			usuario.setSenha(criptografarSenha(usuario.getSenha()));
+			return repository.save(usuario);
+		} else {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Usuário já cadastrado");
+		}
+
+	}
+
+	public Usuario atualizarUsuario (Usuario usuario){
+		Optional<Usuario> optional = repository.findById(usuario.getId());
+			if(optional.isPresent()) {
+				usuario.setSenha(criptografarSenha(usuario.getSenha()));
+				return repository.save(usuario);
+			} else {
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Usuário não encontrado");
+			}
+	}
+	
 	public Optional<UsuarioLogin> logar(Optional<UsuarioLogin> user) {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		Optional<Usuario> usuario = repository.findByUsuario(user.get().getUsuario());
 
 		if (usuario.isPresent()) {
 			if (encoder.matches(user.get().getSenha(), usuario.get().getSenha())) {
-
-				String auth = user.get().getUsuario() + ":" + user.get().getSenha();
-				byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
-				String authHeader = "Basic " + new String(encodedAuth);
-
-				user.get().setToken(authHeader);
+				user.get().setToken(gerarTokenBasic64(user.get().getUsuario(), user.get().getSenha()));
 				user.get().setNome(usuario.get().getNome());
 
 				return user;
 			}
 		}
 		throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Usuário não cadastrado");
+	}
+	
+	public String gerarTokenBasic64(String usuario, String senha) {
+		String auth = usuario + ":" + senha;
+		byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
+		return  "Basic " + new String(encodedAuth);
 	}
 }
